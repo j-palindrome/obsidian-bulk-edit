1a07b471709bf90db57448e434fa9e147ee9f506
Joshua Tazman Reinier <jreinier@oberlin.edu>
2023-05-25 11:58:55 -0400

    syced with monorepo

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,5 @@
-import * as _ from 'lodash'
-import {
-	DropdownComponent,
-	Menu,
-	Modal,
-	Notice,
-	Plugin,
-	Setting,
-	TAbstractFile,
-	TFile,
-	TextComponent,
-} from 'obsidian'
-import { DataArray, DataviewApi, getAPI } from 'obsidian-dataview'
-import invariant from 'tiny-invariant'
-import $ from 'jquery'
+import { Menu, Notice, Plugin, TAbstractFile } from 'obsidian'
+import { getAPI } from 'obsidian-dataview'
 import MetadataWranglerModal from './MetadataWranglerModal'
 import RegexModal from './RegexModal'
 
@@ -20,40 +7,40 @@ interface MySettings {}
 const DEFAULT_SETTINGS: MySettings = {}
 
 export default class MetadataWrangler extends Plugin {
-	settings: MySettings
+  settings: MySettings
 
-	async onload() {
-		const dv = getAPI()
-		if (!dv) {
-			new Notice('Please install Dataview to use Metadata Wrangler')
-			return
-		}
+  async onload() {
+    const dv = getAPI()
+    if (!dv) {
+      new Notice('Please install Dataview to use Metadata Wrangler')
+      return
+    }
 
-		await this.loadSettings()
+    await this.loadSettings()
 
-		this.registerEvent(
-			app.workspace.on('file-menu', (menu: Menu, file: TAbstractFile) => {
-				menu.addItem((item) =>
-					item
-						.setTitle('Wrangle Metadata')
-						.setIcon('layout-list')
-						.onClick(() => new MetadataWranglerModal(file).open())
-				)
-				menu.addItem((item) =>
-					item
-						.setTitle('Regex Find & Replace')
-						.setIcon('search')
-						.onClick(() => new RegexModal(file).open())
-				)
-			})
-		)
-	}
+    this.registerEvent(
+      app.workspace.on('file-menu', (menu: Menu, file: TAbstractFile) => {
+        menu.addItem((item) =>
+          item
+            .setTitle('Wrangle Metadata')
+            .setIcon('layout-list')
+            .onClick(() => new MetadataWranglerModal(file).open())
+        )
+        menu.addItem((item) =>
+          item
+            .setTitle('Regex Find & Replace')
+            .setIcon('search')
+            .onClick(() => new RegexModal(file).open())
+        )
+      })
+    )
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData())
-	}
+  async loadSettings() {
+    this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData())
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings)
-	}
+  async saveSettings() {
+    await this.saveData(this.settings)
+  }
 }
